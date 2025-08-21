5791d8af2e0e73474f32c089e188cb0f311738c0
a-rizak <abdiriiadan095@gmail.com>
2025-08-21 15:23:40 +0300

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~E3VxGJ5K#ARNtMKVZmM7LF8goWpxsO6bL014SzSRfzYVRBCzOoww",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "254727740389", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
