01a05e0507cc89c2095ed1c8e6ed31ad915bb0b1
a-rizak <abdiriiadan095@gmail.com>
2025-08-21 15:03:12 +0300

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
   SESSION_ID: "malvin~En1FGZjK#i0DQBUnTHvsoW84M5F_5VMsPDwrysgylSHnQ3f1lhjc ",  // add your Session Id make sure it starts with malvin~
-  OWNER_NUMBER: "354727740389", // PUT OWNER NUMBER HERE
+  OWNER_NUMBER: "254727740389", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
